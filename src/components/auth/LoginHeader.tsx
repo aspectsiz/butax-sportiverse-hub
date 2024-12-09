056fe5ab13098916f5773202f7e05ca6fa138d2c
aspectsiz <81068489+aspectsiz@users.noreply.github.com>
2024-12-09 16:57:56 +0300

    Update LoginHeader.tsx
    I made the design a little more similar,

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const LoginHeader = () => (
   <header className="bg-white mb-2">
-    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="max-w-3xl mx-auto">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <img
