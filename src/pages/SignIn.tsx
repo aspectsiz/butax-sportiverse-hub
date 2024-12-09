5ec4bd7edc4c0d5d2181f5f43ca5a99043146765
aspectsiz <81068489+aspectsiz@users.noreply.github.com>
2024-12-09 16:45:33 +0300

    Update SignIn.tsx
    I made the design a little more similar,

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -6,9 +6,9 @@ import { LoginFooter } from "@/components/auth/LoginFooter";
 
 const SignIn = () => {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center">
       
-      <main className="flex-grow container max-w-md mx-auto px-4 py-8">
+      <main className="container max-w-md mx-auto px-4 py-8">
         
         <div className="bg-white p-6 rounded-lg shadow-md">
           <LoginHeader />
