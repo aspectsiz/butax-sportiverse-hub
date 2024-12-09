754175d7062b7eba64741441c6f31a97959a79c9
aspectsiz <81068489+aspectsiz@users.noreply.github.com>
2024-12-09 16:58:54 +0300

    Update SignIn.tsx
    I made the design a little more similar,

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -8,7 +8,7 @@ const SignIn = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center">
       
-      <main className="container max-w-md mx-auto px-4 py-8">
+      <main className="container max-w-3xl mx-auto px-4 py-8">
         
         <div className="bg-white p-6 rounded-lg shadow-md">
           <LoginHeader />
