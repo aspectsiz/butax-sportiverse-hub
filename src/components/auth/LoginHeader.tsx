22e22885c825a2070ac18fbd33888396e89b4546
aspectsiz <81068489+aspectsiz@users.noreply.github.com>
2024-12-10 17:48:15 +0300

    Update LoginHeader.tsx
    or contunie with added

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -21,5 +21,15 @@ export const LoginHeader = () => (
         </div>
       </div>
     </div>
+       <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-white px-2 text-gray-500">
+            Or continue with
+          </span>
+        </div>
+      </div>
   </header>
 );
