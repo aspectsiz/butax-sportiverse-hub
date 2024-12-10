bd1441deae71b0d623194c82d2f9cc65d3f35cb4
aspectsiz <81068489+aspectsiz@users.noreply.github.com>
2024-12-10 17:49:05 +0300

    Update LoginHeader.tsx
    Or contunie with deleted

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -21,15 +21,5 @@ export const LoginHeader = () => (
         </div>
       </div>
     </div>
-       <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-white px-2 text-gray-500">
-            Or continue with
-          </span>
-        </div>
-      </div>
   </header>
 );
